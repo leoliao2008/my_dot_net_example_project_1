bff0b324769e0dc2cd7aac68e00980ee8055282a
Leo Liao <lliao@midtronics.com>
2024-04-18 17:49:42 +0800

    change dockerfile aspnet:8.0:alpine to aspnet:8.0

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using Microsoft.Data.SqlClient;
 using MinimalApiTutorial;
 using System.Data;
 
+string dbServerDevelop = "47.113.113.91";
+string dbServerProduction = "localhost";
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 app.Map("/", () => { return TypedResults.Ok("Demo Server Launches Successfully!"); });
@@ -10,8 +12,9 @@ app.Map("/", () => { return TypedResults.Ok("Demo Server Launches Successfully!"
 RouteGroupBuilder grpBuilder = app.MapGroup("/student");
 grpBuilder.MapGet("/{id}", GetStudentById);
 
-static async Task<IResult> GetStudentById(int id) {
-    using (IDbConnection conn = new SqlConnection("Server=47.113.113.91; Database=Demo; User ID=leo;Password=Leoliao2008; TrustServerCertificate=True")) {
+ async Task<IResult> GetStudentById(int id) {
+
+    using (IDbConnection conn = new SqlConnection("Server="+dbServerProduction+"; Database=Demo; User ID=leo;Password=Leoliao2008; TrustServerCertificate=True")) {
         Student stu = await conn.QuerySingleAsync<Student>("SELECT * FROM STUDENT WHERE id = @id",new { id = id});
         if (stu is null)
         {
